5a9cd1fdb664e3cbb0232e718f8602c5d6df22f6
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-09-16 11:40:40 -0400

    pushing to development

--- swipeswap/lib/screens/welcome.dart
+++ swipeswap/lib/screens/welcome.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 /// Welcome page
+/// "By the students, for the students"
+/// "Savor the flavor, fund the future"
+/// "Start Swapping!"
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
