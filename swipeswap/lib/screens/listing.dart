0732810ca7c0129539252fad2fd07a3f06fce840
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-09-17 00:09:49 -0400

    I hate committing with broken code

--- swipeswap/lib/screens/listing.dart
+++ swipeswap/lib/screens/listing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:swipeswap/utils/constants.dart';
 import 'package:swipeswap/widgets/custom_nav_bar.dart';
 import 'package:swipeswap/widgets/dining_court.dart';
@@ -19,10 +20,13 @@ class _ListingState extends State<Listing> {
       body: Form(
         child: Column(
           children: [
+            Text(
+              "Exchange your swipe",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
+            ),
+            // Dining court
             DropdownButtonFormField(
                 value: diningCourt,
-                decoration:
-                    const InputDecoration(hintText: "Exchange your swipe"),
                 items: [
                   for (DiningCourt diningCourt in DiningCourt.values)
                     DropdownMenuItem(
@@ -38,8 +42,19 @@ class _ListingState extends State<Listing> {
                     diningCourt = value ?? diningCourt;
                   });
                 }),
-            // TODO: good font
             // TODO: price
+            TextFormField(
+              controller: _maxPriceController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: "Max base price (not accounting for delivery)",
+              ),
+              validator: Validatorless.multiple([
+                Validatorless.min(1, 'The price must be greater than 1'),
+                Validatorless.max(20, 'The price must be less than 20'),
+                Validatorless.required("Price cannot be blank!")
+              ]),
+            ),
             // TODO: canDeliver
             // TODO: delivery fee (appears if can deliver)
             // TODO: button
