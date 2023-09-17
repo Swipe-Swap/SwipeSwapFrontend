8e21f09b0f241bb9bddfab0bb9bd99fd6c70b531
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-09-17 00:47:31 -0400

    worked on listing

--- swipeswap/lib/provider/listing_provider.dart
+++ swipeswap/lib/provider/listing_provider.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:swipeswap/models/user.dart';
 import 'package:swipeswap/utils/constants.dart';
 
 class ListingProvider extends ChangeNotifier {
   DiningCourt? diningCourt;
+  SwapUser? seller;
 
   void setDiningCourt(DiningCourt court) {
     diningCourt = court;
+    notifyListeners();
+  }
+
+  void setSeller(SwapUser swapUser) {
+    seller = swapUser;
     notifyListeners();
   }
 }
--- swipeswap/lib/screens/listing.dart
+++ swipeswap/lib/screens/listing.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
+import 'package:swipeswap/models/listing.dart';
+import 'package:swipeswap/provider/listing_provider.dart';
+import 'package:swipeswap/provider/user_provider.dart';
 import 'package:swipeswap/utils/constants.dart';
 import 'package:swipeswap/widgets/custom_nav_bar.dart';
 import 'package:swipeswap/widgets/dining_court.dart';
+import 'package:validatorless/validatorless.dart';
 
 class Listing extends StatefulWidget {
   const Listing({super.key});
@@ -13,6 +18,12 @@ class Listing extends StatefulWidget {
 
 class _ListingState extends State<Listing> {
   String diningCourt = DiningCourt.earhart.toString();
+  TextEditingController _rangeMilesController = TextEditingController();
+  TimeOfDay _time =
+      TimeOfDay.fromDateTime(DateTime.now().add(const Duration(hours: 1)));
+  TextEditingController _milePriceController = TextEditingController();
+  TextEditingController _basePriceController = TextEditingController();
+  List<bool> _delivery = [true, false];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,12 +53,15 @@ class _ListingState extends State<Listing> {
                     diningCourt = value ?? diningCourt;
                   });
                 }),
-            // TODO: price
+            // Base price
             TextFormField(
-              controller: _maxPriceController,
+              controller: _basePriceController,
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: "Max base price (not accounting for delivery)",
+              decoration: InputDecoration(
+                hintText:
+                    "Base price for your swipe (not accounting for delivery)",
+                hintStyle:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
               ),
               validator: Validatorless.multiple([
                 Validatorless.min(1, 'The price must be greater than 1'),
@@ -55,9 +69,89 @@ class _ListingState extends State<Listing> {
                 Validatorless.required("Price cannot be blank!")
               ]),
             ),
-            // TODO: canDeliver
-            // TODO: delivery fee (appears if can deliver)
-            // TODO: button
+            // Expiration time
+            Text(
+              "How long do you want the listing to be available?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() async {
+                    _time = await showDialog(
+                        context: context,
+                        builder: (context) =>
+                            TimePickerDialog(initialTime: _time));
+                  });
+                },
+                child: Text(_time.toString())),
+            // Delivery boolean
+            Text(
+              "Would you be willing to deliver?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
+            ),
+            ToggleButtons(
+              isSelected: const [true, false],
+              children: const [Text("Pickup"), Text("Deliver")],
+              onPressed: (value) {
+                setState(() {
+                  _delivery[value] = !_delivery[value];
+                });
+              },
+            ),
+            // Delivery fee
+            Text(
+              "Set your delivery fee (per mile)",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
+            ),
+            (_delivery[0])
+                ? TextFormField(
+                    controller: _milePriceController,
+                    keyboardType: TextInputType.number,
+                    validator: Validatorless.multiple([
+                      Validatorless.min(1, 'The price must be greater than 1'),
+                      Validatorless.max(10, 'The price must be less than 20'),
+                      Validatorless.required("Price cannot be blank!")
+                    ]),
+                  )
+                : const Placeholder(),
+            // Range Miles
+            Text(
+              "How far are you willing to go to deliver?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.sp),
+            ),
+            TextFormField(
+              controller: _rangeMilesController,
+              keyboardType: TextInputType.number,
+            ),
+            // Submission Button
+            ElevatedButton(
+              onPressed: () {
+                // Submit data
+                db.collection("listings").add(
+                      SwapListing.toJson(
+                        SwapListing(
+                          basePrice:
+                              int.tryParse(_basePriceController.value.text) ??
+                                  5,
+                          canDeliver: _delivery[0],
+                          diningCourt: diningCourt,
+                          initial: null,
+                          // NOTE: hopefully this works
+                          expiration: DateTime.tryParse(_time.toString()),
+                          milePrice:
+                              int.tryParse(_milePriceController.text) ?? 5,
+                          rangeMiles:
+                              int.tryParse(_rangeMilesController.text) ?? 1,
+                          sellerId: Provider.of<ListingProvider>(context,
+                                  listen: false)
+                              .seller
+                              ?.uuid,
+                        ),
+                      ),
+                    );
+              },
+              child: const Text("Submit?"),
+            ),
           ],
         ),
       ),
